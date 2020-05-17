5de6a12ce44a0208c5e6670d49784e67bcd833ee
SmallSquare <958503508@qq.com>
2020-05-17 17:44:05 +0800

    Add a tip.

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def test():
     # database.del_all("movie")
     # database.del_all("comment")
     # database.insert_movie(spider.getMoviesInfor(10))
-    # database.insert_comment(spider.getMovieShortComments(26885074, 2), 26885074)
+    # database.insert_comment(spider.getMovieShortComments(32659890, 20), 32659890)
+    # # No more than 10 pages. getMovieShortComments()方法不要爬10页以上，因为豆瓣限制了短评页数。
     # print(database.get_movies())
     # print(database.get_comments(26885074))
     pass
